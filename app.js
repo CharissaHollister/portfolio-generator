19dc7d214d144acad66bbdb6807715934d4d1874
CharissaHollister <charissa.hollister@diasorin.com>
2022-06-04 20:10:21 -0500

    lesson 3

--- app.js
+++ app.js
@@ -1,79 +1,168 @@
+const inquirer = require("inquirer");
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username",
+    },
+    {
+      type: "input",
+      name: "about",
+      message: "Provide some information about yourself:",
+    },
+  ]);
+};
+
+const promptProject = () => {
+  console.log(`
+=================
+Add a New Project
+=================
+`);
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the name of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Provide a description of the project (Required)",
+    },
+    {
+      type: "checkbox",
+      name: "languages",
+      message: "What did you build this project with? (Check all that apply)",
+      choices: [
+        "JavaScript",
+        "HTML",
+        "CSS",
+        "ES6",
+        "jQuery",
+        "Bootstrap",
+        "Node",
+      ],
+    },
+    {
+      type: "input",
+      name: "link",
+      message: "Enter the GitHub link to your project. (Required)",
+    },
+    {
+      type: "confirm",
+      name: "feature",
+      message: "Would you like to feature this project?",
+      default: false,
+    },
+    {
+      type: "confirm",
+      name: "confirmAddProject",
+      message: "Would you like to enter another project?",
+      default: false,
+    },
+  ]);
+};
+
+promptUser()
+  .then((answers) => console.log(answers))
+  .then(promptProject)
+  .then((projectAnswers) => console.log(projectAnswers));
+
 const fs = require("fs");
+const generatePage = require("./src/page-template");
 
-const profileDataArgs = process.argv.slice(2);
+const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
-
-const generatePage = require("./src/page-template.js");
-
-fs.writeFile("./index.html", generatePage(name, github), (err) => {
-  if (err) throw new Error(err);
+fs.writeFile("./index.html", pageHTML, (err) => {
+  if (err) throw err;
 
   console.log("Portfolio complete! Check out index.html to see the output!");
 });
 
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
 // const fs = require("fs");
 
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
-//same as:
+// const profileDataArgs = process.argv.slice(2);
+
 // const [name, github] = profileDataArgs;
-// console.log(profileDataArgs);
 
-// const generatePage = (userName, githubName) =>
-//   `Name: ${userName}, Github: ${githubName}`;
+// const generatePage = require("./src/page-template.js");
 
-// const generatePage = (userName, githubName) => {
-//   return `
-//     Name: ${userName}
-//     GitHub: ${githubName}
-//   `;
-// // };
-
-// const generatePage = (name, github) => {
-//   return `
-//   <!DOCTYPE html>
-//   <html lang="en">
-//   <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//     <title>Portfolio Demo</title>
-//   </head>
-
-//   <body>
-//     <h1>${name}</h1>
-//     <h2><a href="https://github.com/${github}">Github</a></h2>
-//   </body>
-//   </html>
-//   `;
-// };
-
-// fs.writeFile("index.html", generatePage(name, github), (err) => {
-//   if (err) throw err;
+// fs.writeFile("./index.html", generatePage(name, github), (err) => {
+//   if (err) throw new Error(err);
 
 //   console.log("Portfolio complete! Check out index.html to see the output!");
 // });
 
-// const printProfileData = (profileDataArr) => {
-//   console.log(profileDataArr);
+// // const profileDataArgs = process.argv.slice(2, process.argv.length);
+// // const fs = require("fs");
+
+// // const name = profileDataArgs[0];
+// // const github = profileDataArgs[1];
+// //same as:
+// // const [name, github] = profileDataArgs;
+// // console.log(profileDataArgs);
+
+// // const generatePage = (userName, githubName) =>
+// //   `Name: ${userName}, Github: ${githubName}`;
+
+// // const generatePage = (userName, githubName) => {
+// //   return `
+// //     Name: ${userName}
+// //     GitHub: ${githubName}
+// //   `;
+// // // };
+
+// // const generatePage = (name, github) => {
+// //   return `
+// //   <!DOCTYPE html>
+// //   <html lang="en">
+// //   <head>
+// //     <meta charset="UTF-8">
+// //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+// //     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+// //     <title>Portfolio Demo</title>
+// //   </head>
+
+// //   <body>
+// //     <h1>${name}</h1>
+// //     <h2><a href="https://github.com/${github}">Github</a></h2>
+// //   </body>
+// //   </html>
+// //   `;
 // // };
 
-// // Notice the lack of parentheses around the `profileDataArr` parameter?
-// const printProfileData = (profileDataArr) => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
+// // fs.writeFile("index.html", generatePage(name, github), (err) => {
+// //   if (err) throw err;
 
-//   console.log("================");
+// //   console.log("Portfolio complete! Check out index.html to see the output!");
+// // });
 
-//   // Is the same as this...
-//   //   profileDataArr.forEach((profileItem) => {
-//   //     console.log(profileItem);
-//   //   });
-//   // Is the same as this...
+// // const printProfileData = (profileDataArr) => {
+// //   console.log(profileDataArr);
+// // // };
+
+// // // Notice the lack of parentheses around the `profileDataArr` parameter?
+// // const printProfileData = (profileDataArr) => {
+// //   // This...
+// //   for (let i = 0; i < profileDataArr.length; i += 1) {
+// //     console.log(profileDataArr[i]);
+// //   }
+
+// //   console.log("================");
+
+// //   // Is the same as this...
+// //   //   profileDataArr.forEach((profileItem) => {
+// //   //     console.log(profileItem);
+// //   //   });
+// //   // Is the same as this...
 //   profileDataArr.forEach((profileItem) => console.log(profileItem));
 // };
 
