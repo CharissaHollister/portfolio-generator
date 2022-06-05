a77eec1e85138a35cbe0b184812777bdcdd7764b
CharissaHollister <charissa.hollister@diasorin.com>
2022-06-04 19:00:53 -0500

    sectoin 2

--- app.js
+++ app.js
@@ -1,31 +1,83 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require("fs");
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+const generatePage = require("./src/page-template.js");
+
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
+  if (err) throw new Error(err);
+
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const fs = require("fs");
+
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+//same as:
+// const [name, github] = profileDataArgs;
 // console.log(profileDataArgs);
+
+// const generatePage = (userName, githubName) =>
+//   `Name: ${userName}, Github: ${githubName}`;
+
+// const generatePage = (userName, githubName) => {
+//   return `
+//     Name: ${userName}
+//     GitHub: ${githubName}
+//   `;
+// // };
+
+// const generatePage = (name, github) => {
+//   return `
+//   <!DOCTYPE html>
+//   <html lang="en">
+//   <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//     <title>Portfolio Demo</title>
+//   </head>
+
+//   <body>
+//     <h1>${name}</h1>
+//     <h2><a href="https://github.com/${github}">Github</a></h2>
+//   </body>
+//   </html>
+//   `;
+// };
+
+// fs.writeFile("index.html", generatePage(name, github), (err) => {
+//   if (err) throw err;
+
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
 
 // const printProfileData = (profileDataArr) => {
 //   console.log(profileDataArr);
+// // };
+
+// // Notice the lack of parentheses around the `profileDataArr` parameter?
+// const printProfileData = (profileDataArr) => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+
+//   console.log("================");
+
+//   // Is the same as this...
+//   //   profileDataArr.forEach((profileItem) => {
+//   //     console.log(profileItem);
+//   //   });
+//   // Is the same as this...
+//   profileDataArr.forEach((profileItem) => console.log(profileItem));
 // };
 
-
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = (profileDataArr) => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-
-  console.log("================");
-
-  // Is the same as this...
-  //   profileDataArr.forEach((profileItem) => {
-  //     console.log(profileItem);
-  //   });
-  // Is the same as this...
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-
-
-printProfileData(profileDataArgs);
-
+// printProfileData(profileDataArgs);
 
 // // var is function-scoped, so redeclaring it in a block will cause its value outside the block to change as well:
 
@@ -49,7 +101,6 @@ printProfileData(profileDataArgs);
 
 // console.log(two); // prints 'two: declared outside the block', because two declared inside the block is a separate variable. The 'let' variables are unrelated and therefore are unaffected by each other.
 
-
 // // var is function-scoped, so changing its value in a block causes its value in the outer environment to change as well:
 
 // var three = 'three: declared outside the block';
@@ -71,4 +122,3 @@ printProfileData(profileDataArgs);
 // }
 
 // console.log(four); // prints 'four: inside the block', because we didn't redeclare the variable inside the block. That meant we referenced the variable outside the block, and we therefore changed it.
-
